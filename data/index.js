68fb8a12f1e75fb34649d2dadec5d0f53a550564
Jared <f7.z7ro@gmail.com>
2025-03-08 16:53:50 -0600

    2nd commit

--- data/index.js
+++ data/index.js
@@ -1,13 +1,8 @@
-//import { navigation } from "./common/navigation"
-
-//import { index as indexPageData} from "./pages/index"
-//import { contactus as contactUsPageData } from "./pages/contactus"
 import { rankings as rankingData } from "./pages/rankings";
 
 export const getPageContext = (pagePath)=>{
     console.log("Page to Load Context:", pagePath)
     const commonVariables = {
-        //...rankingPage
     }
 
     let pageVariables = {};
@@ -16,8 +11,6 @@ export const getPageContext = (pagePath)=>{
         case '/index.html':
             pageVariables = rankingData
             break;
-        /*case '/contactus.html':
-            pageVariables = contactUsPageData*/
     }
     const finalContext = {
         ...commonVariables,
--- data/pages/rankings.js
+++ data/pages/rankings.js
@@ -2,21 +2,49 @@ export const rankings = {
     RankingFutbol: [
         {
             "posicion": 1,
-            "nombre": "FC Barcelona",
-            "partidos_jugados": 50,
-            "partidos_ganados": 40,
+            "nombre": "Olimpia",
+            "partidos_jugados": 10,
+            "partidos_ganados": 7,
             "partidos_empatados": 2,
-            "goles_favor": 70,
-            "goles_contra": 40
+            "goles_favor": 17,
+            "goles_contra": 5
         },
         {
             "posicion": 2,
-            "nombre": "Real Madrid",
-            "partidos_jugados": 20,
-            "partidos_ganados": 10,
+            "nombre": "Real España",
+            "partidos_jugados": 10,
+            "partidos_ganados": 7,
+            "partidos_empatados": 1,
+            "goles_favor": 17,
+            "goles_contra": 9
+        },
+        {
+            "posicion": 3,
+            "nombre": "FC Motagua",
+            "partidos_jugados": 10,
+            "partidos_ganados": 4,
+            "partidos_empatados": 3,
+            "goles_favor": 17,
+            "goles_contra": 10
+        },
+        {
+            "posicion": 4,
+            "nombre": "Club Deportivo Genesis",
+            "partidos_jugados": 10,
+            "partidos_ganados": 3,
             "partidos_empatados": 6,
-            "goles_favor": 40,
-            "goles_contra": 30
-        }
+            "goles_favor": 13,
+            "goles_contra": 7
+        },
+        {
+            "posicion": 5,
+            "nombre": "Victoria",
+            "partidos_jugados": 10,
+            "partidos_ganados": 4,
+            "partidos_empatados": 3,
+            "goles_favor": 10,
+            "goles_contra": 8
+        },
+
     ]
 }
